589caa8ec780698e319f998e6a6019535046d527
Ereduvyge <35508222+Ereduvyge@users.noreply.github.com>
2022-02-07 13:42:31 +0300

    Update models.py

--- agregator/models.py
+++ agregator/models.py
@@ -8,7 +8,6 @@ class Ad(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     price = models.CharField(max_length=200)
-    photo = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
 
     def publish(self):
         self.save()
